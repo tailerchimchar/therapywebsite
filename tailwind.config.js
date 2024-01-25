f182cfd89403dfdf6b9665f66a5ebe4c8ecc4f2a
Tailer Nguyen <35902625+tailerchimchar@users.noreply.github.com>
2024-01-24 19:13:40 -0600

    Adding tailwind code and daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
