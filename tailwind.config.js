992d48024dd9ecbaef183c5e601f5098a74bf0f0
Tailer Nguyen <tailernguyen@microsoft.com>
2024-01-24 21:36:18 -0600

    Revert "Adding tailwind code and daisyUI"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: [],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 }
 
